63ee93412611d458543dda8f56e096e287fa0171
luigicareddu <luigi.careddu@gmail.com>
2014-12-26 18:23:27 +0100

    updated note/show to implement item insertion and editing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-
-  # GET /items
-  # GET /items.json
-  def index
-    @items = Item.all
-  end
-
-  # GET /items/1
-  # GET /items/1.json
-  def show
-  end
+  before_action :set_note
 
   # GET /items/new
   def new
@@ -24,23 +14,17 @@ class ItemsController < ApplicationController
   # POST /items
   # POST /items.json
   def create
-    @note = Note.find(params[:note_id])
     @item = @note.items.create(item_params)
-
     redirect_to note_path(@note)
   end
 
   # PATCH/PUT /items/1
   # PATCH/PUT /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to note_path(@note), notice: 'Item was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -48,16 +32,17 @@ class ItemsController < ApplicationController
   # DELETE /items/1.json
   def destroy
     @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to note_path(@note), notice: 'Item was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
+    end
+
+    def set_note
+      @note = Note.find(params[:note_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/views/items/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @item, :id, :text, :user_id, :note_id, :created_at, :updated_at
